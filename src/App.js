3dcd98d31fd874e710554dc302ff3dfdc723ee50
Peter Daino Otieno <peterdaino0@gmail.com>
2022-05-11 10:15:01 +0300

    --

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
+import Hero from './components/atoms/Hero';
 
 function App() {
   return (
-    <div className='h-screen content-center w-full items-center bg-blue-400'>
-      <h1 className="text-3xl font-bold underline text-blue-600">
-      Hello world!
-    </h1>
-    </div>
+    <>
+    <Hero/>
+    </>
     
   )
 }
---
+++ src/components/atoms/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='flex h-screen w-full items-center bg-blue-400'>
+      <h1 className="text-3xl font-bold underline text-blue-600">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default Hero;
