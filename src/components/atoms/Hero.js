f3fb77ef88ae7e87f44b6672784c2bde2d0759f2
Peter Daino Otieno <peterdaino0@gmail.com>
2022-05-11 15:38:49 +0300

    hero section created

--- src/components/atoms/Hero.js
+++ src/components/atoms/Hero.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import HeroImg from '../../assets/imgs/hero-img.png';
 
 function Hero() {
   return (
-    <div className='flex h-screen w-full items-center bg-blue-400'>
-      <h1 className="text-3xl font-bold underline text-blue-600">
-      Hello world!
-    </h1>
+    <div className='flex flex-col md:flex-row items-center content-center h-screen w-full'>
+        <div className='flex flex-col md:w-6/12 border border-black'>
+        <h1 className="text-4xl text-left font-bold text-blue-600">
+            Presta USSD
+        </h1>
+        <p className='text-blue-600 text-left'>Guarantee loans Anywhere and Anytime</p>
+        </div>
+        <div className='hidden md:flex border w-6/12 border-black'>
+        <img className='w-full h-full bg-center' src={HeroImg} alt='hero_img'/>
+        </div>
     </div>
   )
 }
