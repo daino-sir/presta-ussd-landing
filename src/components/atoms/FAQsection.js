0111a6ec318bb4faf4352b2b4a7946c836759d45
Peter Daino Otieno <peterdaino0@gmail.com>
2022-05-13 11:10:53 +0300

    created FAQ

--- src/components/atoms/FAQsection.js
+++ src/components/atoms/FAQsection.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { FAQdata } from '../../data/data';
+import Faq from "react-faq-component";
+import { config } from '../../data/constants/config';
 
 function FAQsection() {
   return (
-    <div className='h-screen w-full'>
-      <div className='h-60 w-full flex flex-col'>
+    <div className='flex flex-col justify-center h-screen w-full'>
+      <div className='h-60 w-full flex flex-col px-12'>
+      <Faq
+        data={FAQdata}
+        styles={styles}
+        config={config}
+      />
         </div>
     </div>
       );
 }
+const styles = {
+  // bgColor: 'white',
+  titleTextColor: "#3A73BE",
+  rowTitleColor: "blue",
+  // rowContentColor: 'grey',
+  // arrowColor: "red",
+};
 
 export default FAQsection;
