ea08c84fc78a17ada056bf37a7ccb7711f5aefc4
Peter Daino Otieno <peterdaino0@gmail.com>
2022-05-13 15:43:45 +0300

    UI cleanup

--- src/components/atoms/SectionTwo.js
+++ src/components/atoms/SectionTwo.js
@@ -5,10 +5,10 @@ import ImageTwo from '../../assets/imgs/_section_two_mob.png';
 function SectionTwo() {
   return (
     <>
-    <div className='flex mt- w-full yellow justify-center items-center'>
+    <div className='flex yellow md:mt-96 justify-center items-center'>
      <div className='w-4/5 h-4/5'>
-     <img alt='section_two' src={ImageOne} className='hidden md:flex object-cover rounded-3xl'/>  
-     <img alt='section_two' src={ImageTwo} className='flex md:hidden object-cover rounded-3xl'/>  
+     <img alt='section_two' src={ImageOne} className='hidden md:flex object-cover md:mt-48 rounded-3xl'/>  
+     <img alt='section_two' src={ImageTwo} className='flex md:hidden object-cover mt-40 rounded-3xl'/>  
     </div>
      </div>
     </>
--- src/components/atoms/footer.js
+++ src/components/atoms/footer.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { footerData } from '../../data/data';
+import Faq from "react-faq-component";
+import { config } from '../../data/constants/config';
 
 function Footer() {
   return (
@@ -19,26 +22,23 @@ function Footer() {
             </div>
             </div>
       </div>
-      <div className='flex flex-col w-full items-center justify-evenly md:w-1/5 px-6 md:px-0'>
-            <p className='text-white text-lg'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ullamcorper morbi vel montes, ipsum ultricies mi nunc, scelerisque ac. Lacus facilisis porttitor aliquam quam consectetur. Diam ultrices sem purus felis lectus ipsum, id gravida. Ac egestas ante in adipiscing in euismod ullamcorper elementum quis. Urna pretium sed lacus elit blandit non sed ultricies. In sed aliquam ultrices amet, enim nunc, ultrices viverra phasellus.
-            </p>
-            <div className='flex flex-col'>
-            <h1 className='text-white text-lg'>
-              Follow Us
-            </h1>
-            <div className='flex flex-row w-full justify-between'>
-              <div className='bg-gray-300 w-16 h-16 rounded-full'></div>
-              <div className='bg-gray-300 w-16 h-16 rounded-full'></div>
-              <div className='bg-gray-300 w-16 h-16 rounded-full'></div>
-              <div className='bg-gray-300 w-16 h-16 rounded-full'></div>
-            </div>
-            </div>
+      <div className='flex flex-col w-full md:mt-24 md:w-1/5 px-6 md:px-0'>
+        <Faq
+          data={footerData}
+          styles={styles}
+          config={config}
+        />
       </div>
           <div className='flex flex-col w-full items-center md:w-1/5'><h1 className='text-white text-lg'>3</h1></div>
       </div>
 
   )
 }
-
+const styles = {
+  bgColor: '#3A73BE',
+  titleTextColor: 'white',
+  rowTitleColor: "white",
+  rowContentColor: 'white',
+  // arrowColor: "red",
+};
 export default Footer;
