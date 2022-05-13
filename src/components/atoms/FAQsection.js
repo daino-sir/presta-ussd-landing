1aa5ccad6a1bb696717222ee639e7b564581a1cd
Peter Daino Otieno <peterdaino0@gmail.com>
2022-05-13 12:11:21 +0300

    --

--- src/components/atoms/FAQsection.js
+++ src/components/atoms/FAQsection.js
@@ -6,7 +6,7 @@ import { config } from '../../data/constants/config';
 function FAQsection() {
   return (
     <div className='flex flex-col justify-center h-screen w-full'>
-      <div className='h-60 w-full flex flex-col px-12'>
+      <div className='h-60 w-full flex flex-col px-12 md:px-36'>
       <Faq
         data={FAQdata}
         styles={styles}
