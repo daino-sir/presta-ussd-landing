31be95a1f934edc4a6f44616713a415a6da4093b
Peter Daino Otieno <peterdaino0@gmail.com>
2022-06-03 00:33:53 +0300

    --

--- src/components/atoms/footer.js
+++ src/components/atoms/footer.js
@@ -7,11 +7,11 @@ function Footer() {
   return (
       <div className='flex flex-col-reverse md:flex-row h-screen justify-evenly blue'>
           <div className='flex flex-col w-full items-center justify-evenly md:w-1/5 px-6 md:px-0'>
-            <p className='text-white text-lg'>
+            <p className='text-white font-thin text-lg'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ullamcorper morbi vel montes, ipsum ultricies mi nunc, scelerisque ac. Lacus facilisis porttitor aliquam quam consectetur. Diam ultrices sem purus felis lectus ipsum, id gravida. Ac egestas ante in adipiscing in euismod ullamcorper elementum quis. Urna pretium sed lacus elit blandit non sed ultricies. In sed aliquam ultrices amet, enim nunc, ultrices viverra phasellus.
             </p>
             <div className='flex flex-col'>
-            <h1 className='text-white text-lg'>
+            <h1 className='font-thin text-white text-lg'>
               Follow Us
             </h1>
             <div className='flex flex-row w-full justify-between'>
@@ -22,7 +22,7 @@ function Footer() {
             </div>
             </div>
       </div>
-      <div className='flex flex-col w-full md:mt-24 md:w-1/5 px-6 md:px-0'>
+      <div className='font-thin flex flex-col w-full md:mt-24 md:w-1/5 px-6 md:px-0'>
         <Faq
           data={footerData}
           styles={styles}
