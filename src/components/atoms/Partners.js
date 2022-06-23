a7c4aaf4e2956f6e0762bb7049d9229a1282d5fe
Peter Daino Otieno <peterdaino0@gmail.com>
2022-06-23 16:16:51 +0300

    revision_1

--- src/components/atoms/Partners.js
+++ src/components/atoms/Partners.js
@@ -1,23 +1,37 @@
 import React from 'react';
-import ImageTwo from '../../assets/imgs/_section_one2.png';
-import ImageTwoMob from '../../assets/imgs/_section_one2_mob.png';
+import LogoImarisha from '../../assets/imgs/mask_group_3_1partner_logo.png';
+import LogoFive from '../../assets/imgs/logoipsum-logo-21partner_logo.png';
+import LogoThree from '../../assets/imgs/logoipsum-logo-22partner_logo-1.png';
+import LogoFour from '../../assets/imgs/logoipsum-logo-22partner_logo-2.png';
+import LogoTwo from '../../assets/imgs/logoipsum-logo-22partner_logo-2.png';
 
 function Partners() {
   return (
     <>
-        <div className='h-32 w-full'>
+        <div className='h-32 md:h-44 w-full'>
         <div className='h-full flex flex-col justify-center'>
-        <div className='flex flex-col justify-center'>
+        <div className='flex flex-col md:h-14 justify-center'>
         <h1 className="text-xl md:text-2xl md:text-center blue-text text-center">
             Organisations that already use Presta USSD
         </h1>
         </div>
-        
         <div className='flex flex-row justify-evenly md:w-2/5 md:self-center content-center'>
-              <div className='bg-gray-300 w-16 h-16 rounded-full'></div>
-              <div className='bg-gray-300 w-16 h-16 rounded-full'></div>
-              <div className='bg-gray-300 w-16 h-16 rounded-full'></div>
-              <div className='bg-gray-300 w-16 h-16 rounded-full'></div>
+              <img className='w-16 h-24 md:w-24 md:h-24 rounded-full'
+              src={LogoImarisha}
+              alt='Imarisha_Logo'
+              />
+              <img className='w-16 h-24 md:w-24 md:h-24 rounded-full'
+              src={LogoThree}
+              alt='Imarisha_Logo'
+              />
+              <img className='w-16 h-24 md:w-24 md:h-24 rounded-full'
+              src={LogoFour}
+              alt='Imarisha_Logo'
+              />
+              <img className='w-16 h-24 md:w-24 md:h-24 rounded-full'
+              src={LogoFive}
+              alt='Imarisha_Logo'
+              />
         </div>
         </div>
         
