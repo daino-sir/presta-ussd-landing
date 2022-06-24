b33cf1d3d997580422fb8cd5e80adde47b2a097e
Peter Daino Otieno <peterdaino0@gmail.com>
2022-06-24 12:57:51 +0300

    added subscribe section

--- src/components/atoms/footer.js
+++ src/components/atoms/footer.js
@@ -29,7 +29,12 @@ function Footer() {
           config={config}
         />
       </div>
-          <div className='flex flex-col w-full items-center md:w-1/5'><h1 className='text-white text-lg'>3</h1></div>
+      <div className='md:my-26 flex flex-col-reverse md:flex-row h-36 justify-evenly blue'>
+      <form class="m-4 flex">
+    	  <input class="rounded-l-lg p-4 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white" placeholder="your@mail.com"/>
+		    <button class="px-8 rounded-r-lg bg-yellow-400  text-gray-800 font-bold p-4 uppercase border-yellow-500 border-t border-b border-r">Subscribe</button>
+	    </form>
+      </div>
       </div>
 
   )
