cd6e1b82dc16fa08c73154dcca3704bf56c78f70
Peter Daino Otieno <72624000+daino-sir@users.noreply.github.com>
2022-06-28 04:38:10 +0300

    Update footer wording

--- src/components/atoms/footer.js
+++ src/components/atoms/footer.js
@@ -8,7 +8,7 @@ function Footer() {
       <div className='flex flex-col-reverse md:flex-row h-screen justify-evenly blue'>
           <div className='flex flex-col w-full items-center justify-evenly md:w-1/5 px-6 md:px-0'>
             <p className='text-white font-thin text-lg'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ullamcorper morbi vel montes, ipsum ultricies mi nunc, scelerisque ac. Lacus facilisis porttitor aliquam quam consectetur. Diam ultrices sem purus felis lectus ipsum, id gravida. Ac egestas ante in adipiscing in euismod ullamcorper elementum quis. Urna pretium sed lacus elit blandit non sed ultricies. In sed aliquam ultrices amet, enim nunc, ultrices viverra phasellus.
+            Grow your business, unlock new efficiencies and create happier & more loyal customers with Presta
             </p>
             <div className='flex flex-col'>
             <h1 className='font-thin text-white text-lg'>
