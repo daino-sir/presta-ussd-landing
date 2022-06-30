dcd3c0a9fcf1afc4ec57fc0ede12b614fb124280
Peter Daino Otieno <72624000+daino-sir@users.noreply.github.com>
2022-06-30 16:06:33 +0300

    --fixed horizontal scroll bug

--- src/components/atoms/footer.js
+++ src/components/atoms/footer.js
@@ -29,12 +29,12 @@ function Footer() {
           config={config}
         />
       </div>
-      <div className='md:my-26 flex flex-col-reverse md:flex-row h-36 justify-evenly blue'>
-      <form class="m-4 flex">
-    	  <input class="rounded-l-lg p-4 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white" placeholder="your@mail.com"/>
+      {/* <div className='md:my-26 flex flex-col-reverse md:flex-row h-36 w-full justify-evenly blue'>
+      <form class="my-4 flex-row">
+    	  <input class="rounded-l-lg p-4 border-t mr-0 border-b border-l h-14 text-gray-800 border-gray-200 width-full bg-white" placeholder="your@mail.com"/>
 		    <button class="px-8 rounded-r-lg bg-yellow-400  text-gray-800 font-bold p-4 uppercase border-yellow-500 border-t border-b border-r">Subscribe</button>
 	    </form>
-      </div>
+      </div> */}
       </div>
 
   )
